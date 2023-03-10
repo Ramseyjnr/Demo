f13d010c111ffb79adaac7c393388298d643ef25
Ramseyjnr <48526389+Ramseyjnr@users.noreply.github.com>
2023-03-09 22:56:33 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+//require module
+const express = require ('express');
+const morgan = require('morgan');
+const methodOverride = require ('method-override');
+const storyRoutes = require('./routes/storyRoutes');
+
+
+
+
+
+
+
+
+//create app
+const app = express();
+
+
+
+
+
+//configure app
+let port = 3000;
+let host = 'localhost';
+app.set ('view engine', 'ejs');
+
+
+
+
+//mount middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+
+
+
+
+//set up router
+app.get('/',(reg, res)=>{
+    res.render('index');
+});
+
+app.use('/stories', storyRoutes);
+
+app.use((req, res, next)=>{
+    let err = new Error ('The server cannot locate' + req.url);
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next)=>{
+    console.log(err.stack);
+    if (!err.status) {
+        err.status = 500;
+        err.message = ("internal Server Error");
+    }
+
+    res.status(err.status);
+    res.render('error', {error: err});
+});
+
+
+//start ther server
+app.listen(port, host, ()=>{
+    console.log('server is running on port', port);
+
+})
